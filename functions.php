c2767b47f122c6fd241236c2e1a20c591dec02ba
Evan-457 <pieraevan@gmail.com>
2024-01-16 16:28:34 +0100

    essais ACF

--- functions.php
+++ functions.php
@@ -13,18 +13,31 @@
     //css
     //var_dump(get_template_directory_uri());
     //all
-    wp_enqueue_style('reset', get_template_directory_uri().'/css/reset.css');
-    wp_enqueue_style('style', get_template_directory_uri().'/css/style.css');
-    wp_enqueue_style('font', get_template_directory_uri().'/css/font.css');
-    wp_enqueue_style('all', get_template_directory_uri().'/css/all.css');
-    //header
-    wp_enqueue_style('header', get_template_directory_uri().'/css/header.css');
-    wp_enqueue_style('nav', get_template_directory_uri().'/css/nav.css');
-    //pages
-    wp_enqueue_style('page-home', get_template_directory_uri().'/css/page-home.css');
-    wp_enqueue_style('page-contact', get_template_directory_uri().'/css/page-contact.css');
-    //footer
-    wp_enqueue_style('footer', get_template_directory_uri().'/css/footer.css');
-    //js
-    wp_enqueue_script('nav-js', get_template_directory_uri() . '/js/nav.js', array(), '1.0.0', array("strategy" => "defer"));
-?>
+    if (!is_admin()) {
+        wp_enqueue_style('reset', get_template_directory_uri().'/css/reset.css');
+        wp_enqueue_style('style', get_template_directory_uri().'/css/style.css');
+        wp_enqueue_style('font', get_template_directory_uri().'/css/font.css');
+        wp_enqueue_style('all', get_template_directory_uri().'/css/all.css');
+        //header
+        wp_enqueue_style('header', get_template_directory_uri().'/css/header.css');
+        wp_enqueue_style('nav', get_template_directory_uri().'/css/nav.css');
+        //pages
+        wp_enqueue_style('page-home', get_template_directory_uri().'/css/page-home.css');
+        wp_enqueue_style('page-contact', get_template_directory_uri().'/css/page-contact.css');
+        //footer
+        wp_enqueue_style('footer', get_template_directory_uri().'/css/footer.css');
+        //js
+        wp_enqueue_script('nav-js', get_template_directory_uri() . '/js/nav.js', array(), '1.0.0', array("strategy" => "defer"));
+    }
+
+    function p($args){
+        echo '<pre>';
+        var_dump($args);
+        echo '</pre>';
+      }
+      
+      
+      function d($args){
+        p($args);
+        die();
+      }
---
+++ layouts/citation.php
@@ -0,0 +1,9 @@
+<?php
+    p($content);
+    echo($content['text']);
+?>    
+    <img src="<?php echo($content['img']['url']); ?>">
+
+<?php
+    echo($content['name']);
+?>
