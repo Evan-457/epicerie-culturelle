d31853c2130663750714e3c31d5deb63d1c9b71d
Evan-457 <pieraevan@gmail.com>
2024-01-17 14:52:20 +0100

    modifs nav

--- template/nav.php
+++ template/nav.php
@@ -8,12 +8,25 @@
     </div>
     
   </a>
-  <ul>
-    <li><a href="#">A propos</a></li>
-    <li><a href="#">Nos services</a></li>
-    <li><a href="#">Témoignages</a></li>
-    <li><a href="#">Contact</a></li>
-  </ul>
+  <div class="nav-align">
+    <div class="nav-entry">
+      <a href="#">A propos</a>
+      <a href="#">Nos services</a>
+      <a href="#">Témoignages</a>
+      <a href="#">Contact</a>
+    </div>
+    <div class="nav-social">
+      <a href="">
+          <img class="logo-social" src="<?php echo(get_template_directory_uri()) ?>/img/logo-instagram.svg" alt="logo d'instagram">
+      </a>
+      <a href="">
+          <img class="logo-social" src="<?php echo(get_template_directory_uri()) ?>/img/logo-facebook.svg" alt="logo de facebook">
+      </a>
+      <a href="">
+          <img class="logo-social" src="<?php echo(get_template_directory_uri()) ?>/img/logo-youtube.svg" alt="logo de YouTube">
+      </a>
+    </div>
+  </div>
 </div>
 
 
